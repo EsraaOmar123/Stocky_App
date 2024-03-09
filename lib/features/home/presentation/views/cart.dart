431e4c77f56bbcdafcfe1799f58e6a8274c81f54
Nader <ns1568@fayoum.edu.eg>
2024-03-09 11:16:28 +0200

    Add sweatshirt image and details to cart view

--- lib/features/home/presentation/views/cart.dart
+++ lib/features/home/presentation/views/cart.dart
@@ -4,6 +4,7 @@ import 'package:gap/gap.dart';
 import 'package:project1/features/home/presentation/manager/app%20cubit/app_cubit.dart';
 import 'package:swipe_to/swipe_to.dart';
 
+import '../../../../core/utils/assets.dart';
 import '../../../../core/widgets/flutter_toast.dart';
 
 class CartScreen extends StatelessWidget {
@@ -18,67 +19,63 @@ class CartScreen extends StatelessWidget {
               message: 'delete from cart successfully',
               state: ToastState.warning);
         }
-        // TODO: implement listener
       },
       builder: (context, state) {
-        return AppCubit.get(context).products.isEmpty
-            ? const Center(child: Text('cart is Empty'))
-            : ListView.separated(
-                padding: const EdgeInsets.all(15),
-                itemBuilder: (context, index) => SwipeTo(
-                  onRightSwipe: (v) {
-                    AppCubit.get(context).deleteData(
+        return ListView.separated(
+          padding: const EdgeInsets.all(15),
+          itemBuilder: (context, index) => SwipeTo(
+            onRightSwipe: (v) {
+              /*AppCubit.get(context).deleteData(
                         id: AppCubit.get(context).products[index]['id'],
-                        table: 'cart');
-                  },
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 150,
-                        height: 150,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          image: DecorationImage(
-                              image: AssetImage(
-                            AppCubit.get(context).products[index]['image'],
-                          )),
-                        ),
-                      ),
-                      const Gap(15),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(
-                            AppCubit.get(context).products[index]['title'],
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(fontSize: 19),
-                          ),
-                          Text(
-                            AppCubit.get(context).products[index]
-                                ['description'],
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(color: Colors.grey),
-                          ),
-                          Text(
-                            AppCubit.get(context).products[index]['price'],
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(color: Colors.orange),
-                          ),
-                        ],
-                      ),
-                    ],
+                        table: 'cart');*/
+            },
+            child: Row(
+              children: [
+                Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: const DecorationImage(
+                        image: AssetImage(
+                      Assets.imagesSweatshirt,
+                    )),
                   ),
                 ),
-                separatorBuilder: (context, index) => const Gap(20),
-                itemCount: AppCubit.get(context).products.length,
-              );
+                const Gap(15),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'sweatshirt',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium!
+                          .copyWith(fontSize: 19),
+                    ),
+                    Text(
+                      'nice sweatshirt with a good quality ',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium!
+                          .copyWith(color: Colors.grey),
+                    ),
+                    Text(
+                      50.toString(),
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium!
+                          .copyWith(color: Colors.orange),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          separatorBuilder: (context, index) => const Gap(20),
+          itemCount: 10,
+        );
       },
     );
   }
