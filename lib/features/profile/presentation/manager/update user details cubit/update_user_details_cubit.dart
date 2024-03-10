b0553a3d5390d7027d3a49b3eced55da77eda7e1
Nader <ns1568@fayoum.edu.eg>
2024-03-10 19:58:43 +0200

    Remove unused code

--- lib/features/profile/presentation/manager/update user details cubit/update_user_details_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'update_user_details_state.dart';
-
-class UpdateUserDetailsCubit extends Cubit<UpdateUserDetailsState> {
-  UpdateUserDetailsCubit() : super(UpdateUserDetailsInitial());
-}
